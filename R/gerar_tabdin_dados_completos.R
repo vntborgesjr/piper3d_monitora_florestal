b8328ac696cc315feac96ef4f7df028d339139b6
Vitor Borges Jr <vntborgesjr@gmail.com>
2023-04-03 23:50:48 -0300

    Create gerar_tabdin_dados_completos.R

---
+++ R/gerar_tabdin_dados_completos.R
@@ -0,0 +1,23 @@
+# Descrição
+# gera tabela dinâmica dos dados completos
+
+gerar_tabdin_dados_completos <- function(
+    dados = readr::read_rds(
+      file = paste0(
+        stringr::str_remove(
+          getwd(), 
+          pattern = "doc"
+        ),
+        '/data/dados_completos.rds'
+      )
+    ),
+    n_linhas = 1:4500
+) {
+  # gerar tabela dinâmica dos dados completos
+  dados_completos <- dados |> 
+    slice(n_linhas) |> 
+    DT::datatable(filter = list(position = "top"))
+  
+  # retornar a tabela dinamica dos dados completos
+  return(dados_completos)
+}
