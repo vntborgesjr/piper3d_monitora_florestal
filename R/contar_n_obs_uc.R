bd6e93eda7e1e0dba6478dc679889954a101b7f5
Vitor Borges Jr <vntborgesjr@gmail.com>
2023-04-06 11:59:00 -0300

    Create contar_n_obs_uc.R

---
+++ R/contar_n_obs_uc.R
@@ -0,0 +1,40 @@
+# Descrição
+# gera uma tabela com o número de observações válidas para cada UC.
+
+contar_n_obs_uc <- function(
+    dados = readr::read_rds(
+      file = paste0(
+        stringr::str_remove(
+          getwd(), 
+          "doc"
+        ),
+        "/data/dados_selecionados.rds"
+      )
+    )
+) {
+  # gerar a tabela com o número de observações por UC
+  n_obs_uc <- dados  |>  
+    dplyr::count(
+      uc_name,
+      uc_name_abv
+    )
+  
+  # grava a tabela n_obs_uc.rds no diretório data/
+  readr::write_rds(
+    n_obs_uc,
+    file = paste0(
+      stringr::str_remove(
+        getwd(), 
+        "doc"
+      ),
+      "/data/n_obs_uc.rds"
+    )
+  )
+  
+  # retorna a tabela com o número de observações por UC
+  return(n_obs_uc)
+}
+
+# Exemplo
+
+
