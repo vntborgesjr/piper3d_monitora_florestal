1bda5c3a2e20bb9d762792118cddc397e1526512
Vitor Borges Jr <vntborgesjr@gmail.com>
2023-04-06 12:00:38 -0300

    Create plotar_n_obs_validadas.R

---
+++ R/plotar_n_obs_validadas.R
@@ -0,0 +1,35 @@
+# Descrição
+# gera gráfico com número de observações validadas para cada nível taxonômico
+
+plotar_n_obs_validadas <- function(
+    dados = readr::read_rds(
+      stringr::str_replace_all(
+        getwd(),
+        pattern = "doc",
+        replacement = "data/tabela_n_obs_validadas.rds"
+      )
+    )
+) {
+  #
+  grafico_n_sp_validada <- dados  |>  
+    ggplot2::ggplot() +
+    ggplot2::aes(
+      x = validation,
+      y = n,
+      label = n
+    ) +
+    ggplot2::geom_col(
+      fill = "chartreuse4"
+    ) +
+    ggplot2::geom_label() +
+    ggplot2::labs(
+      title = "Número de obs. validadas para \ncada nível taxonômico",
+      x = "Nível taxonômico",
+      y = "Contagem"
+    ) +
+    ggplot2::theme_minimal(14)
+  
+  return(grafico_n_sp_validada)
+}
+
+# Exemplo
