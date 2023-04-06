fd7837b8bdb11d863a567a10c53709592a5d816e
Vitor Borges Jr <vntborgesjr@gmail.com>
2023-04-06 11:58:41 -0300

    Update carregar_dados_selecionados.R

--- R/carregar_dados_selecionados.R
+++ R/carregar_dados_selecionados.R
@@ -1,26 +1,32 @@
 # Descrição
-# a função carregar_dados1() carrega toda a planilha de dados 
-# selecionando automaticamente as colunas CDU, Local - Nome da Unidade de 
-# Conservação
-# deve carregar uma planilha que esteja no formato do arquivo:
-# Planilha Oficial consolidada de Masto-aves 2014-21 Validada CEMAVE CPB CENAP
-# enviada pelo Gerson por whatsapp no dia 08/03/2023
-
+# a função carregar_dados_selecionados() carrega os dados_completos para
+# gerar a tabel dados_selecionados
 carregar_dados_selecionados <- function(
     dados = readr::read_rds(
-      file = paste0(
-        stringr::str_remove(
-          getwd(), 
-          pattern = "doc"
-        ),
-        '/data/dados_completos.rds'
-      )
+    file = paste0(
+      stringr::str_remove(
+        getwd(), 
+        "doc"
+      ),
+      "/data/dados_completos.rds"
     )
+  )
     ) {
-  
   # gerar o data.frame desejado
   dados_selecionados <- dados |> 
     filter(validation == "Espécie")
+  
+  # grava uma versão dados_completos.rds no diretório data
+  readr::write_rds(
+    dados_selecionados,
+    file = paste0(
+      stringr::str_remove(
+        getwd(), 
+        "doc"
+      ),
+      "/data/dados_selecionados.rds"
+    )
+  )
   
   # retornar o data.frame
   return(dados_selecionados)
