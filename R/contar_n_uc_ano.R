ef155a784b0c1ba875ccf6d972907415722c4dfd
Vitor Borges Jr <vntborgesjr@gmail.com>
2023-04-06 11:59:19 -0300

    Create contar_n_uc_ano.R

---
+++ R/contar_n_uc_ano.R
@@ -0,0 +1,52 @@
+# Descrição
+# fornece o número total de observações por espécie.
+
+contar_n_uc_ano <- function(
+    dados = readr::read_rds(
+      file = paste0(
+        stringr::str_remove(
+          getwd(), 
+          "doc"
+        ),
+        "/data/dados_selecionados.rds"
+      )
+    )
+) {
+  # gera o número de observaçẽos por espécie
+  n_uc_ano <- dados |>  
+    dplyr::filter(
+      validation == "Espécie"
+    ) |> 
+    dplyr::count(
+      year, 
+      uc_name
+    ) |>  
+    dplyr::group_by(
+      year
+    ) |>  
+    dplyr::count(
+      uc_name
+    ) |>  
+    dplyr::summarise(
+      n_ucs = sum(n)
+    )
+  
+  # grava a tabela n_uc_ano.rds no diretório data/
+  readr::write_rds(
+    n_uc_ano,
+    file = paste0(
+      stringr::str_remove(
+        getwd(), 
+        "doc"
+      ),
+      "/data/n_uc_ano.rds"
+    )
+  )
+  
+  # retorna o número de UCs por ano
+  return(n_uc_ano)
+}
+
+# Exemplo
+
+#contar_total_sp() 
