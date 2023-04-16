4131a4251ecc79c5297f9b2dae4ebf952d7e05be
Vitor Borges Jr <vntborgesjr@gmail.com>
2023-04-16 20:21:15 -0300

    Create gerar_tabdin_n_uc_ano.R

---
+++ R/gerar_tabdin_n_uc_ano.R
@@ -0,0 +1,25 @@
+# Descrição
+# gera tabela dinâmica do número de UCs amostradas por ano
+
+gerar_tabdin_n_uc_ano <- function(
+    dados = readr::read_rds(
+      file = paste0(
+        stringr::str_remove(
+          getwd(), 
+          pattern = "doc"
+        ),
+        '/data/n_uc_ano.rds'
+      )
+    )
+) {
+  # gerar tabela dinâmica dos dados do número de observações por sp
+  tabdin_n_uc_ano <- dados |> 
+    DT::datatable(
+      filter = list(
+        position = "top"
+      )
+    )
+  
+  # retornar a tabela dinamica do número de observações por sp
+  return(tabdin_n_uc_ano)
+}
