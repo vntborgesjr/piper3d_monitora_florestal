e70e3c75c70f9bfbd01f09485d1fd5f2431be502
Vitor Borges Jr <vntborgesjr@gmail.com>
2023-04-28 14:54:41 -0300

    Update transformar_para_distanceR_covariaveis.R

--- R/transformar_para_distanceR_covariaveis.R
+++ R/transformar_para_distanceR_covariaveis.R
@@ -1,14 +1,9 @@
-# Descrição
-# deve receber dados de uma espécie e UC('s) pré-selecionadas e 
 # transforma para o formato para análise no pacote distance do R
 # adicionando duas covariáveis
 transformar_para_distanceR_covariaveis <- function(
     dados = readr::read_rds(
       file = paste0(
-        stringr::str_remove(
-          getwd(), 
-          pattern = "doc"
-        ),
+        here::here(),
         '/data/dados_completos.rds'
       )
     )
@@ -18,6 +13,8 @@ transformar_para_distanceR_covariaveis <- function(
       Region.Label = `ea_name`,
       Sample.Label = `sampling_day`,
       Effort = day_effort,
+      sp_name,
+      sp_name_abv,
       distance,
       year,
       size = group_size,
@@ -48,19 +45,3 @@ transformar_para_distanceR_covariaveis <- function(
   return(dados_transformados_dist_r_cov)
 }
 
-# exemplo de uso da função
-# lembre-se de especificar o diretório correto onde o arquivo se encontra
-# na sua máquina
-#source("R/carregar_dados_completos.R")
-#source("R/carregar_dados_filtrados.R")
-
-#dados1 <- carregar_dados_completos("/home/usuario/Documentos/Vitor/Piper3D/WWF/Monitora/Dados/data-raw/Planilha Oficial consolidada de Masto-aves 2014-21 Validada CEMAVE CPB CENAP.xlsx")
-#dados2 <- carregar_dados_filtrados(dados = "/home/usuario/Documentos/Vitor/Piper3D/WWF/Monitora/Dados/data-raw/Planilha Oficial consolidada de Masto-aves 2014-21 Validada CEMAVE CPB CENAP.xlsx",
- #                                  nome_uc = "Resex Tapajós-Arapiuns",
-  #                                 nome_sp = "Dasyprocta croconota")
-
-#dados3 <- filtra_trasforma_para_distanceR(dados1)
-#View(dados3)  
-
-#dados4 <- transforma_para_distanceR2(dados2)
-#View(dados4)
