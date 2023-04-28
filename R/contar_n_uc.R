05ebde3e7560bbd4de76b43f7f39dfa334c9983e
Vitor Borges Jr <vntborgesjr@gmail.com>
2023-04-28 14:52:18 -0300

    Update contar_n_uc.R

--- R/contar_n_uc.R
+++ R/contar_n_uc.R
@@ -1,23 +1,17 @@
-# Descrição
-# fornece o número total de UCs na base de dados.
-
+# fornece o número total de UCs na base de dados
 contar_n_uc <- function(
     dados = readr::read_rds(
       file = paste0(
-        stringr::str_remove(
-          getwd(), 
-          "doc"
-        ),
+        here::here(),
         "/data/dados_completos.rds"
       )
     )
 ) {
   #
   n_ucs <- dados |> 
-    distinct(uc_name) |> 
+    dplyr::distinct(uc_name) |> 
     nrow()
   
   return(n_ucs)
 }
 
-# Exemplo
