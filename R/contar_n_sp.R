13ec153f27f9b8d180a9419c914ce56dc049e660
Vitor Borges Jr <vntborgesjr@gmail.com>
2023-04-28 14:52:14 -0300

    Update contar_n_sp.R

--- R/contar_n_sp.R
+++ R/contar_n_sp.R
@@ -1,20 +1,16 @@
 # Descrição
-# fornece o número total de UCs na base de dados.
-
+# fornece o número total de espécies na base de dados
 contar_n_sp <- function(
     dados = readr::read_rds(
       file = paste0(
-        stringr::str_remove(
-          getwd(), 
-          "doc"
-        ),
+        here::here(),
         "/data/dados_completos.rds"
       )
     )
 ) {
   #
   n_sp <- dados |> 
-    distinct(sp) |> 
+    dplyr::distinct(sp_name) |> 
     nrow()
   
   return(n_sp)
