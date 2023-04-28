47364e148213475425820199906694b1c68439b5
Vitor Borges Jr <vntborgesjr@gmail.com>
2023-04-28 14:52:45 -0300

    Update gerar_tabdin_n_ano_uc.R

--- R/gerar_tabdin_n_ano_uc.R
+++ R/gerar_tabdin_n_ano_uc.R
@@ -1,13 +1,8 @@
-# Descrição
 # gera tabela dinâmica do número de anos em que cada UC foi amostrada
-
 gerar_tabdin_n_ano_uc <- function(
     dados = readr::read_rds(
       file = paste0(
-        stringr::str_remove(
-          getwd(), 
-          pattern = "doc"
-        ),
+        here::here(),
         '/data/n_ano_uc.rds'
       )
     )
