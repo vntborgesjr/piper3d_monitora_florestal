4a3ac8f0529aa1031a37924ed9fc7d1c37fa4f05
Vitor Borges Jr <vntborgesjr@gmail.com>
2023-04-28 14:51:47 -0300

    Update contar_n_obs_sp.R

--- R/contar_n_obs_sp.R
+++ R/contar_n_obs_sp.R
@@ -1,42 +1,30 @@
 # Descrição
 # fornece o número total de observações por espécie.
-
 contar_n_obs_sp <- function(
     dados = readr::read_rds(
       file = paste0(
-        stringr::str_remove(
-          getwd(), 
-          "doc"
-        ),
-        "/data/dados_selecionados.rds"
+        here::here(),
+        "/data/dados_completos.rds"
       )
     )
 ) {
-  # gera o número de observaçẽos por espécie
+  #
   n_obs_sp <- dados |> 
-    # gerar nome das sp abreviados
-    # mutate(sp_abv = ) |> 
     dplyr::count(
-      sp,
-      #sp_abv
+      sp_name,
+      sp_name_abv
     )
   
-  # grava a tabela n_obs_sp.rds no diretório data/
+  # grava a tabela n_uc_ano.rds no diretório data/
   readr::write_rds(
     n_obs_sp,
     file = paste0(
-      stringr::str_remove(
-        getwd(), 
-        "doc"
-      ),
+      here::here(),
       "/data/n_obs_sp.rds"
     )
   )
   
-  # retorna o número de observaçẽos por espécie
   return(n_obs_sp)
 }
 
 # Exemplo
-
-#contar_total_sp() 
