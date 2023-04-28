c2fa19210eda781313dcca2fd356d22935177ccc
Vitor Borges Jr <vntborgesjr@gmail.com>
2023-04-28 14:52:54 -0300

    Update gerar_tabdin_n_obs_sp_uc.R

--- R/gerar_tabdin_n_obs_sp_uc.R
+++ R/gerar_tabdin_n_obs_sp_uc.R
@@ -1,13 +1,8 @@
-# Descrição
 # gera tabela dinâmica do número de observações por espécies por UC
-
 gerar_tabdin_n_obs_sp_uc <- function(
     dados = readr::read_rds(
       file = paste0(
-        stringr::str_remove(
-          getwd(), 
-          pattern = "doc"
-        ),
+        here::here(),
         '/data/n_obs_sp_uc.rds'
       )
     )
